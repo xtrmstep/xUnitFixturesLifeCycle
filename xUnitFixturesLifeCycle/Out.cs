9f076ba078f2e4594cfe4669bac399b09eaf1253
Alexander Goida <alexander.goida@gmail.com>
2015-07-02 11:49:59 +0300

    xUnit life cycle test code

---
+++ xUnitFixturesLifeCycle/Out.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitFixturesLifeCycle
+{
+    static class Out
+    {
+        public static void Show(this string text)
+        {
+            Debug.WriteLine(text);
+        }
+    }
+}
---
+++ xUnitFixturesLifeCycle/xUnitArtifacts.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xUnitFixturesLifeCycle
+{
+    public class CollectionFixture : IDisposable
+    {
+        public CollectionFixture()
+        {
+            "CollectionFixture : ctor".Show();
+        }
+
+        public void Dispose()
+        {
+            "CollectionFixture : disposed".Show();
+        }
+    }
+
+    public class ClassFixture : IDisposable
+    {
+        public ClassFixture()
+        {
+            "ClassFixture : ctor".Show();
+        }
+
+        public void Dispose()
+        {
+            "ClassFixture : disposed".Show();
+        }
+    }
+
+    [CollectionDefinition("ContextOne")]
+    public class TestCollection : ICollectionFixture<CollectionFixture>
+    {
+        public TestCollection()
+        {
+            "TestCollection is never instantiated".Show();
+        }
+    }
+
+    [Collection("ContextOne")]
+    public class TestContainerOne : IClassFixture<ClassFixture>, IDisposable
+    {
+        public TestContainerOne()
+        {
+            "TestContainerOne : ctor".Show();
+        }
+
+        [Fact]
+        public void TestOne()
+        {
+            "TestContainerOne : TestOne".Show();
+        }
+
+        [Fact]
+        public void TestTwo()
+        {
+            "TestContainerOne : TestTwo".Show();
+        }
+
+        public void Dispose()
+        {
+            "TestContainerOne : disposed".Show();
+        }
+    }
+
+    [Collection("ContextOne")]
+    public class TestContainerTwo : IDisposable
+    {
+        public TestContainerTwo()
+        {
+            "TestContainerTwo : ctor".Show();
+        }
+
+        [Fact]
+        public void TestOne()
+        {
+            "TestContainerTwo : TestOne".Show();
+        }
+
+        public void Dispose()
+        {
+            "TestContainerTwo : disposed".Show();
+        }
+    }
+
+    public class TestContainerThree
+    {
+        [Fact]
+        public void TestOne()
+        {
+            "TestContainerThree : TestOne".Show();
+        }
+    }
+}
